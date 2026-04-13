1a7fe0f99ddee9f3fbab42d30bb14b313481282f
hiranorm <hira.euclid.norm.root2@gmail.com>
2026-04-13 23:05:44 +0900

    お引っ越し！

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,92 @@
-export const SITE = {
-  website: "https://yurudeep.com/",
-  author: "ひらノルム",
-  profile: "https://github.com/hiranorm",
-  desc: "ゆるふわなディープラーニングブログ",
-  title: "ゆるディープ",
-  ogImage: "astropaper-og.jpg",
-  lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-  showArchives: true,
-  showBackButton: true, // show back button in post detail
-  editPost: {
-    enabled: true,
-    text: "ページを編集",
-    url: "https://github.com/hiranorm/yurudeep/edit/main/",
-  },
-  dynamicOgImage: true,
-  dir: "ltr",
-  lang: "ja",
-  timezone: "Asia/Tokyo",
-} as const;
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "ゆるディープ",
+	subtitle: "ゆるふわなディープラーニングブログ",
+	lang: "ja",
+	themeColor: {
+		hue: 250,
+		fixed: false,
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png",
+		position: "center",
+		credit: {
+			enable: false,
+			text: "",
+			url: "",
+		},
+	},
+	toc: {
+		enable: true,
+		depth: 2,
+	},
+	favicon: [
+		{
+			src: "/favicon/favicon-light-192.png",
+			theme: "light",
+			sizes: "192x192",
+		},
+		{
+			src: "/favicon/favicon-dark-192.png",
+			theme: "dark",
+			sizes: "192x192",
+		},
+		{
+			src: "/favicon/favicon-light-32.png",
+			theme: "light",
+			sizes: "32x32",
+		},
+		{
+			src: "/favicon/favicon-dark-32.png",
+			theme: "dark",
+			sizes: "32x32",
+		},
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/hiranorm/yurudeep",
+			external: true,
+		},
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "/assets/images/logo.png",
+	name: "ひらノルム",
+	bio: "機械学習・AI・自動化についてゆるふわに書いています。",
+	links: [
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/hiranorm",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
