2b200dba2758fd1bd6c14e8a1d445a4cc3773854
hiranorm <hira.euclid.norm.root2@gmail.com>
2026-04-17 19:13:18 +0900

    経歴追加、カテゴリ整理

--- src/config.ts
+++ src/config.ts
@@ -69,7 +69,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "/assets/images/logo.png",
 	name: "ひらノルム",
-	bio: "機械学習・AI・自動化についてゆるふわに書いています。",
+	bio: "京大大学院（工学・生物化学）修了。機械学習・AI・自動化についてゆるふわに書いています。\nKaggle Competitions Expert（銀2・銅4）／Notebooks Expert（金3・銀2・銅3）",
 	links: [
 		{
 			name: "GitHub",
